3d7ac377bd7f78c5dc9c3f67dd126c7d56da4c1e
Selma Suvalija <selma.suvalija@sematext.com>
2023-08-12 20:10:11 +0200

    Initial Commit

---
+++ YahooScraper/YahooScraperApp.swift
@@ -0,0 +1,17 @@
+//
+//  YahooScraperApp.swift
+//  YahooScraper
+//
+//  Created by Selma Suvalija on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct YahooScraperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
